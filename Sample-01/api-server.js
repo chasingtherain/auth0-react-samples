070e6e77df654cb92b248f3ad65b7a1430d9a868
ChasingRain <junpeng94@gmail.com>
2023-06-29 16:34:29 +0800

    enable server to check user's permission and grant/deny access to resource accordingly

--- Sample-01/api-server.js
+++ Sample-01/api-server.js
@@ -5,11 +5,12 @@ const helmet = require("helmet");
 const { auth } = require("express-oauth2-jwt-bearer");
 const authConfig = require("./src/auth_config.json");
 const axios = require('axios');
-
+var request = require("request");
 const app = express();
 const port = process.env.API_PORT || 3001;
 const appPort = process.env.SERVER_PORT || 3000;
 const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
+
 
 if (
   !authConfig.domain ||
@@ -66,6 +67,33 @@ app.get("/api/v2/clients", checkJwt, (req, res) => {
 
 });
 
+app.get("/api/v2/users/{id}/roles", checkJwt, (req, res) => {
+  
+  const token = req.auth.token
+
+  let config = {
+    method: 'get',
+    url: 'https://dev-zwmgj06ga0j7dtc5.us.auth0.com/api/v2/users/{id}/roles',
+    headers: { 
+      'Accept': 'application/json', 
+      'Authorization': `Bearer ${token}`
+    }
+  };
+
+  axios(config)
+  .then((response) => {
+    res.json({ message: 'called get user role API successfully', result: response.data });
+  })
+  .catch((error) => {
+    console.log(error);
+    res.send({
+      msg: "failed to call get user role API", 
+      error: error
+    });
+  });
+
+});
+
 app.get("/api/v2/actions/actions", checkJwt, (req, res) => {
   
   const token = req.auth.token
@@ -89,6 +117,20 @@ app.get("/api/v2/actions/actions", checkJwt, (req, res) => {
       msg: "failed to call get Actions API", 
       error: error
     });
+  });
+
+});
+
+app.get('/get-report-api-token', function (req, res) {
+
+  var options = { method: 'POST',
+  url: 'https://dev-zwmgj06ga0j7dtc5.us.auth0.com/oauth/token',
+  headers: { 'content-type': 'application/json' },
+  body: '{"client_id":"T0tO4mQPnnN9xn6TrUYgrnh3adtSDW1n","client_secret":"7-PP2SZVMAMsVvWhnM9OwzkLHUVyBGhoFHfJlCD2znREF2c0KIPjQDvKIwV8sGsq","audience":"http://localhost:8080/api/v2/","grant_type":"client_credentials"}' };
+
+  request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+      res.json(response.body);
   });
 
 });
--- Sample-01/own-api-server.js
+++ Sample-01/own-api-server.js
@@ -11,13 +11,31 @@ const jwtCheck = auth({
     tokenSigningAlg: 'RS256'
 });
   
+var request = require("request");
 
 app.use(cors({ origin: 'http://localhost:3000' }))
 // enforce on all endpoints
 app.use(jwtCheck);
 
-app.get('/authorized', function (req, res) {
-    res.status(200).json({status:200, msg:'User has access to secured resource'});
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded request bodies
+app.use(express.urlencoded({ extended: true }));
+
+
+app.post('/authorized', function (req, res) {
+    const permission = req.body
+
+    // if arr is not empty, user has access
+    const authorizedUser = permission.filter(item => item.permission_name === 'read:action-report')
+    console.log("authorizedUser: ", authorizedUser)
+    if (authorizedUser.length > 0){
+        res.status(200).json({status:200, msg:'User has access to secured resource'});
+    }
+    else{
+        res.status(401).json({status:401, msg:'Access denied'});
+    }
 });
 
 app.listen(port);
