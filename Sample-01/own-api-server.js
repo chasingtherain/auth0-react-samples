f7ff405c6f93fc31e3e8fbb82d12ee3704a05ca2
ChasingRain <junpeng94@gmail.com>
2023-06-29 14:53:58 +0800

    cleaned up unused imports and functions in server files

--- Sample-01/own-api-server.js
+++ Sample-01/own-api-server.js
@@ -2,21 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const { auth } = require('express-oauth2-jwt-bearer');
-var request = require("request");
 
 const port = process.env.PORT || 8080;
-
-
-// var options = { method: 'POST',
-//   url: 'https://dev-zwmgj06ga0j7dtc5.us.auth0.com/oauth/token',
-//   headers: { 'content-type': 'application/json' },
-//   body: '{"client_id":"T0tO4mQPnnN9xn6TrUYgrnh3adtSDW1n","client_secret":"7-PP2SZVMAMsVvWhnM9OwzkLHUVyBGhoFHfJlCD2znREF2c0KIPjQDvKIwV8sGsq","audience":"http://localhost:8080/api/v2/","grant_type":"client_credentials"}' };
-
-// request(options, function (error, response, body) {
-//   if (error) throw new Error(error);
-
-//   console.log(body);
-// });
 
 const jwtCheck = auth({
     audience: 'http://localhost:8080/api/v2/',
